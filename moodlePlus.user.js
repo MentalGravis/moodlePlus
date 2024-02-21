53b49bdd5b07473629f4cac371f216940b7e6fe9
Mental Gravis <74249422+MentalGravis@users.noreply.github.com>
2024-02-21 12:50:59 +0100

    test0.11

--- moodlePlus.user.js
+++ moodlePlus.user.js
@@ -1,20 +1,20 @@
 // ==UserScript==
 // @name         moodlePlus
 // @namespace    https://violentmonkey.github.io/
-// @version      0.1
+// @version      0.11
 // @description  Solving moodle tests
 // @author       Mental Gravis
 // @match        https://*elearning.uni-obuda.hu/kmooc/quiz/*
 // @run-at       document-end
 // @icon         https://yt3.googleusercontent.com/ytc/AIf8zZSOeyqxYvSBMIUH8oGQLBrFv70jTTdF5qaTh_UUtg=s900-c-k-c0x00ffffff-no-rj
 // @require      https://code.jquery.com/jquery-3.6.4.min.js#sha256-oP6HI9z1XaZNBrJURtCoUT5SUnxFr8s3BzRl+cbzUq8=
-// @updateURL    https://github.com/MentalGravis/moodlePlus/releases/latest/download/moodlePlus.user.js
+// @updateURL    https://github.com/MentalGravis/moodlePlus/raw/main/moodlePlus.user.js
 // @grant        none
 // ==/UserScript==
 
 (function() {
     jQuery(document).ready(function(){
-        
+        console.log("mep");
 
     });
 })();
