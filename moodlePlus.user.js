9af13d22458a195251b81aacfbacd01d53709ca3
Mental Gravis <74249422+MentalGravis@users.noreply.github.com>
2024-02-21 11:02:37 +0100

    base

---
+++ moodlePlus.user.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name         Terc PLUS
+// @namespace    http://tampermonkey.net/
+// @version      2.5.0
+// @description  Különböző funkciókkal bővíti a TERC-Etalon webalkalmazást
+// @author       Mental Gravis
+// @match        https://www.etalon.terc.hu/browser
+// @run-at       document-end
+// @icon         https://yt3.googleusercontent.com/ytc/AIf8zZSOeyqxYvSBMIUH8oGQLBrFv70jTTdF5qaTh_UUtg=s900-c-k-c0x00ffffff-no-rj
+// @require      https://code.jquery.com/jquery-3.6.4.min.js#sha256-oP6HI9z1XaZNBrJURtCoUT5SUnxFr8s3BzRl+cbzUq8=
+// @updateURL    https://github.com/MentalGravis/moodlePlus/releases/latest/download/moodlePlus.user.js
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    jQuery(document).ready(function(){
+        
+
+    });
+})();
+    
